ae12d47f3f30db01edd52be711fd73fd7410d0ec
MrEnigmamgine "Joshua Mayes <mayes.joshua.d@gmail.com>
2022-05-31 21:00:38 -0400

    Minimum Viable Product

--- scf.js
+++ scf.js
@@ -1,18 +1,18 @@
 import { SCF } from "./scripts/config.js";
 /**
  * Project Outline:
- *    Declare class and create methods specific to this module within
+ *  x Declare class and create methods specific to this module within
  *  x Hook into the dev mode module
- *    Inject html into spell item sheets so that user can declare which class it's from
- *    Capture that data into a flag on the spell item
- *    Add a dropdown filter to the player actor sheet next to the other filters
- *    Probably need to capture that as a flag as well
+ *  x  Inject html into spell item sheets so that user can declare which class it's from
+ *  x  Capture that data into a flag on the spell item
+ *  x  Add a dropdown filter to the player actor sheet next to the other filters
+ *  x  Probably need to capture that as a flag as well
  *    Make the filter actually filter the spell list using the same method as the other filters.
  */
 
 
 class spellClassFilter {
-  static ID = 'spell-class-filter';
+  static ID = 'spell-class-filter-for-5e';
   static CONFIG = SCF;
   // static PATH = 'modules/spell-class-filter-for-5e'
 
@@ -43,10 +43,8 @@ Hooks.on("init", function() {
   spellClassFilter.log(true, spellClassFilter.CONFIG)
 });
 
+// Any time an item sheet is rendered check if it is a spell.  If so add the option to set which class the spell comes from.
 Hooks.on("renderItemSheet5e", async (app, html, data) => {
-  // spellClassFilter.log(true, app);
-  // spellClassFilter.log(true, html);
-  // spellClassFilter.log(true, data);
 
   const item = app.object;
   const type = item.data.type;
@@ -54,22 +52,69 @@ Hooks.on("renderItemSheet5e", async (app, html, data) => {
   const firstChild = spellDetailsDiv.children("h3:first");
 
   if( type == "spell" ) {
-    const spellClassForm = await renderTemplate("modules/spell-class-filter-for-5e/templates/spellClassSelector.hbs", {
+    const spellClassForm = await renderTemplate("modules/spell-class-filter-for-5e/templates/spellClassForm.hbs", {
       SCF: spellClassFilter.CONFIG,
       item,
       flags: item.data.flags
     });
 
-    spellClassFilter.log(true, spellClassForm)
-    spellClassFilter.log(true, spellDetailsDiv)
-    spellClassFilter.log(true, firstChild)
-    
     firstChild.after(spellClassForm)
   }
 
 });
 
+Hooks.on("renderActorSheet5e", async (app, html, data) => {
+  const actor = app.object;
+  const type = actor.data.type;
+  const flags = actor.data.flags;
+  const actorSCFlags = flags[spellClassFilter.ID]
+  
+  if (type == "character"){
+    const spellbook = html.find(".tab.spellbook")
+    const filterList = spellbook.find("ul.filter-list");
+    const firstItem = filterList.children("li.filter-item:first");
+    const itemData = actor.data.items
 
+  const actorClassFilter = await renderTemplate("modules/spell-class-filter-for-5e/templates/actorClassFilter.hbs", {
+    SCF: spellClassFilter.CONFIG,
+    actor,
+    flags: flags,
+    scFlags: actor.data.flags[spellClassFilter.ID]
+  });
+
+  firstItem.before(actorClassFilter)
+  // spellClassFilter.log(true, actor)
+  // spellClassFilter.log(true, filterList)
+  // spellClassFilter.log(true, firstItem)
+
+  const spellList = spellbook.find(".inventory-list")
+  const items = spellList.find(".item")
+  // spellClassFilter.log(false, "spellList" , spellList)
+  items.each(function(){
+    let itemID = ($(this).data("item-id"))
+    let item = (itemData.get(itemID))
+    let itemFlags = item.data.flags
+    let itemSCFlags = itemFlags[spellClassFilter.ID]
+
+    console.log('actorSCFlags',actorSCFlags)
+    console.log('itemSCFlags', itemSCFlags)
+    
+    if(actorSCFlags.classFilter != ''){
+      if (itemSCFlags){
+        if (!(itemSCFlags.parentClass == actorSCFlags.classFilter)){
+          $(this).hide()
+        }
+      }else{
+        $(this).hide()
+      }
+    }
+  })
+
+  }
+
+
+
+})
 
 Hooks.on("ready", function() {
   // console.log("This code runs once core initialization is ready and game data is available.");
--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,18 @@
 export const SCF = {}
 
 SCF.classes = {
-    barbarian : "SPELLCLASSFILTER.ClassBarbarian" 
+    artificer   : "SPELLCLASSFILTER.ClassArtificer",
+    barbarian   : "SPELLCLASSFILTER.ClassBarbarian",
+    bard        : "SPELLCLASSFILTER.ClassBard",
+    cleric      : "SPELLCLASSFILTER.ClassCleric", 
+    druid       : "SPELLCLASSFILTER.ClassDruid",
+    fighter     : "SPELLCLASSFILTER.ClassFighter",
+    monk        : "SPELLCLASSFILTER.ClassMonk",
+    paladin     : "SPELLCLASSFILTER.ClassPaladin",
+    ranger      : "SPELLCLASSFILTER.ClassRanger", 
+    rogue       : "SPELLCLASSFILTER.ClassRogue",
+    sorcerer    : "SPELLCLASSFILTER.ClassSorcerer",
+    warlock     : "SPELLCLASSFILTER.ClassWarlock",
+    wizard      : "SPELLCLASSFILTER.ClassWizard",
+    custom      : "SPELLCLASSFILTER.Custom", 
 }
